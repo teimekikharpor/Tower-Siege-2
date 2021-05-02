8ca9ba2f4deadf11ef11e682757c94af4886cb8e
teimekikharpor <77444056+teimekikharpor@users.noreply.github.com>
2021-05-02 22:19:28 +0530

    Add files via upload

---
+++ js/Block.js
@@ -0,0 +1,36 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        if(this.body.speed < 3){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      }
+      else{
+        World.remove(world, this.body);
+        push();
+        this.Visiblity = this.Visiblity - 1;
+        tint(255,this.Visiblity);
+        //rect(this.rect, this.body.position.x, this.body.position.y, 50, 50);
+        pop();
+      }
+    }
+
+}
